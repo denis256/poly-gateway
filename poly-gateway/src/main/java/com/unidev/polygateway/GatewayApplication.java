9fffe962924d801699c4075887ac4015de98196b
Denis <denis.o@linux.com>
2017-01-10 11:39:11 +0200

    Added jminix web interface

--- poly-gateway/src/main/java/com/unidev/polygateway/GatewayApplication.java
+++ poly-gateway/src/main/java/com/unidev/polygateway/GatewayApplication.java
@@ -1,8 +1,10 @@
 package com.unidev.polygateway;
 
 import com.unidev.platform.j2ee.common.WebUtils;
+import org.jminix.console.servlet.MiniConsoleServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -10,11 +12,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableMongoRepositories
-public class GatewayApplication {
+public class GatewayApplication implements ServletContextInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
@@ -31,6 +36,10 @@ public class GatewayApplication {
 		return new WebUtils();
 	}
 
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.addServlet("JmxMiniConsoleServlet", MiniConsoleServlet.class).addMapping("/jmx/*");
+    }
 
 }
 
