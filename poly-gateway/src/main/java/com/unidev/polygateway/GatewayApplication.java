c4c981306bf53e3a370ec895881b874618d3f4cc
Denis <denis.o@linux.com>
2017-01-10 10:13:45 +0200

    Gateway application class

--- poly-gateway/src/main/java/com/unidev/polygateway/GatewayApplication.java
+++ poly-gateway/src/main/java/com/unidev/polygateway/GatewayApplication.java
@@ -1,18 +1,13 @@
 package com.unidev.polygateway;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.unidev.platform.j2ee.common.WebUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -27,31 +22,15 @@ public class GatewayApplication {
 
 	@LoadBalanced
 	@Bean
-	RestTemplate restTemplate() {
+	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
+	@Bean
+	public WebUtils webUtils() {
+		return new WebUtils();
+	}
+
 
 }
 
-@RestController
-class GatewayController {
-
-	@Autowired
-	private RestTemplate restTemplate;
-
-	public String fallback() {
-		return "Service not available";
-	}
-
-	@HystrixCommand(fallbackMethod = "fallback")
-	@GetMapping("/list")
-	public String list() {
-		System.out.println("Loading list");
-		//restTemplate.exchange("http://TOMATO-SERVICE/tomatoes", HttpMethod.GET, null,   );
-		ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://tomato-service/tomatoes", String.class);
-		System.out.println("Response: " + responseEntity.getStatusCode());
-		return responseEntity.getBody() + "";
-	}
-
-}
