93ce0e16748f5fa0e4888490fd9108656b200c46
Denis <denis.o@linux.com>
2017-01-10 10:49:57 +0200

    Web utils usage

--- poly-gateway/src/main/java/com/unidev/polygateway/GatewayApplication.java
+++ poly-gateway/src/main/java/com/unidev/polygateway/GatewayApplication.java
@@ -6,13 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 @EnableCircuitBreaker
 public class GatewayApplication {
 
