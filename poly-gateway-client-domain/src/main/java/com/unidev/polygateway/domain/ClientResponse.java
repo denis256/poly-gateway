d6703a8dd7ba71065fc0786a25b5e39cde686a64
Denis <denis.o@linux.com>
2017-01-25 16:02:43 +0200

    Updated response domain models  for client response payload usage

--- poly-gateway-client-domain/src/main/java/com/unidev/polygateway/domain/ClientResponse.java
+++ poly-gateway-client-domain/src/main/java/com/unidev/polygateway/domain/ClientResponse.java
@@ -6,6 +6,6 @@ import com.unidev.platform.common.dto.response.ResponseWithVersion;
 /**
  * Response from service
  */
-public class ClientResponse extends ResponseWithVersion<String, ResponseStatus, ClientResponsePayload>  {
+public class ClientResponse<T extends ClientResponsePayload> extends ResponseWithVersion<String, ResponseStatus, T>  {
 
 }
