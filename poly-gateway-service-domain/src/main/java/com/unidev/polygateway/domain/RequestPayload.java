ae41420f4f55d4096c0370b92106e8a8b275d6d6
Denis <denis.o@linux.com>
2017-01-25 15:54:19 +0200

    Service request payload

--- poly-gateway-service-domain/src/main/java/com/unidev/polygateway/domain/RequestPayload.java
+++
@@ -1,7 +0,0 @@
-package com.unidev.polygateway.domain;
-
-/**
- * Request payload object
- */
-public class RequestPayload {
-}
--- poly-gateway-service-domain/src/main/java/com/unidev/polygateway/domain/ServiceRequest.java
+++ poly-gateway-service-domain/src/main/java/com/unidev/polygateway/domain/ServiceRequest.java
@@ -3,18 +3,33 @@ package com.unidev.polygateway.domain;
 
 import com.unidev.platform.common.dto.request.RequestWithVersion;
 
+import java.util.HashMap;
+
 /**
  * Serialized request, passed to appropriate service
+ * Payload - payload obtained from client
+ * Version - gateway version
+ * ClientVersion - version field from client
  */
-public class ServiceRequest<T extends RequestPayload> extends RequestWithVersion<String, T> {
+public class ServiceRequest<T extends ClientRequestPayload> extends RequestWithVersion<String, T> {
 
-    private String service;
+    private String clientVersion;
 
-    public String getService() {
-        return service;
+    private HashMap<String, String[]> headers;
+
+    public String getClientVersion() {
+        return clientVersion;
     }
 
-    public void setService(String service) {
-        this.service = service;
+    public void setClientVersion(String clientVersion) {
+        this.clientVersion = clientVersion;
+    }
+
+    public HashMap<String, String[]> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(HashMap<String, String[]> headers) {
+        this.headers = headers;
     }
 }
