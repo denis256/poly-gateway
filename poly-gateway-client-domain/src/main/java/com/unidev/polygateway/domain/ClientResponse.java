ad171cd623541f6a5cfa2545866fd851243ccc52
Denis <denis.o@linux.com>
2017-01-25 15:58:32 +0200

    Added usage of client response status enum

--- poly-gateway-client-domain/src/main/java/com/unidev/polygateway/domain/ClientResponse.java
+++ poly-gateway-client-domain/src/main/java/com/unidev/polygateway/domain/ClientResponse.java
@@ -6,8 +6,6 @@ import com.unidev.platform.common.dto.response.ResponseWithVersion;
 /**
  * Response from service
  */
-public class ClientResponse extends ResponseWithVersion<String, ClientResponse.Status, ClientResponsePayload>  {
-
-    public enum Status { OK, ERROR}
+public class ClientResponse extends ResponseWithVersion<String, ResponseStatus, ClientResponsePayload>  {
 
 }
