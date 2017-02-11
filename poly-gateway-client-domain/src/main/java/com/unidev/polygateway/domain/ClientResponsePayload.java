932fcdb803f275837450f443ad9f91bbf30c9b47
Denis O <denis@universal-development.com>
2017-02-11 15:55:45 +0200

    Dropping legacy modules

--- poly-gateway-client-domain/src/main/java/com/unidev/polygateway/domain/ClientResponsePayload.java
+++
@@ -1,7 +0,0 @@
-package com.unidev.polygateway.domain;
-
-/**
- * Client response payload object
- */
-public class ClientResponsePayload {
-}
--- poly-gateway-client-domain/src/main/java/com/unidev/polygateway/domain/ResponseStatus.java
+++
@@ -1,9 +0,0 @@
-package com.unidev.polygateway.domain;
-
-/**
- * Backend response status enum
- */
-public enum ResponseStatus {
-
-    OK, ERROR
-}
